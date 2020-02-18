2ece473c602d6bb72fba28e19ddc7a5dac9a8bbe
Max <kozlyakov.m@gmail.com>
2020-02-19 00:58:11 +0700

    Add NotFound and Created

--- src/main/java/com/goodline/pastebin/controller/PasteController.java
+++ src/main/java/com/goodline/pastebin/controller/PasteController.java
@@ -1,11 +1,20 @@
 package com.goodline.pastebin.controller;
 
+import com.goodline.pastebin.exceptions.NotFoundException;
 import com.goodline.pastebin.model.Paste;
 import com.goodline.pastebin.repos.PasteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,17 +44,28 @@ public class PasteController{
 
 
     @PostMapping("/")
-    public String newPaste(@RequestBody Paste paste){
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> newPaste(@RequestBody Paste paste){
         UUID uniqueKey = UUID.randomUUID();
         paste.setHash(String.valueOf(uniqueKey));
 
         repository.save(paste);
-        return "paste has been saved\n"+ uniqueKey+"\nexpire date: "+paste.getExpireDate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/"+uniqueKey));
+        String content = "{ message: Paste has been saved, url: " + uniqueKey;
+        return new ResponseEntity<>(content, headers, HttpStatus.CREATED);
+        //return "paste has been saved\n" + uniqueKey+"\nexpire date: "+ paste.getExpireDate();
     }
 
     @GetMapping("/{hash}")
     public Paste getOne(@PathVariable String hash) {
-        return repository.findByHash(hash);
+        Paste response = repository.findByHash(hash);
+        if(response == null) {
+            return response;
+        }
+        else{
+            throw new NotFoundException();
+        }
     }
 
 }
--- src/main/java/com/goodline/pastebin/repos/PasteRepository.java
+++ src/main/java/com/goodline/pastebin/repos/PasteRepository.java
@@ -10,11 +10,12 @@ import java.util.List;
 
 @Repository
 public interface PasteRepository extends JpaRepository<Paste, Integer> {
+
     Paste findByHash(String hash);
 
     //@Query(value = "SELECT * FROM Paste WHERE isPrivate='false' ORDER BY DESC", nativeQuery = true)
     //List<Paste> findLast10();
+
     List<Paste> findTop10ByIsPrivateOrderByIdDesc(boolean isPrivate);
 
-    //List<Paste> findLast10ByIsPrivateOrderById(boolean isPrivate);
 }
