4318cf69bdce16636236c0b38021537aff31b435
kozlyakov.m <kozlyakov.m@gmail.com>
2020-06-04 19:08:39 +0700

    Clean up

--- src/main/java/com/goodline/pastebin/controller/PasteController.java
+++ src/main/java/com/goodline/pastebin/controller/PasteController.java
@@ -4,67 +4,47 @@ import com.goodline.pastebin.exceptions.NotFoundException;
 import com.goodline.pastebin.model.Paste;
 import com.goodline.pastebin.repos.PasteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.SpringProperties;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.*;
 import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 
 @RestController
 @RequestMapping("/api")
-public class PasteController{
-
-    HashMap<Integer, Paste> pastes = new HashMap<>();
+public class PasteController {
 
     @Autowired
     private PasteRepository repository;
 
-
-
-
-    /*@RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }*/
     @GetMapping
-    public List<Paste> getTen(){
+    public List<Paste> getTen() {
         return repository.findTop10ByIsPrivateOrderByIdDesc(false);
     }
 
 
-
     @PostMapping("/")
-    //@ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> newPaste(@RequestBody Paste paste){
+    public ResponseEntity<String> newPaste(@RequestBody Paste paste) {
         UUID uniqueKey = UUID.randomUUID();
         paste.setHash(String.valueOf(uniqueKey));
 
         repository.save(paste);
         HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create("/"+uniqueKey));
-        String content = "{ \"message\": \"Paste has been saved\", \"hash\": \"" + uniqueKey +"\"";
+        headers.setLocation(URI.create("/" + uniqueKey));
+        String content = "{ \"message\": \"Paste has been saved\", \"hash\": \"" + uniqueKey + "\"";
         return new ResponseEntity<>(content, headers, HttpStatus.CREATED);
-        //return "paste has been saved\n" + uniqueKey+"\nexpire date: "+ paste.getExpireDate();
     }
 
     @GetMapping("/{hash}")
     public Paste getOne(@PathVariable String hash) {
         Paste response = repository.findByHash(hash);
-        if(response == null) {
+        if (response == null) {
             throw new NotFoundException();
-        }
-        else{
+        } else {
             return response;
         }
     }
