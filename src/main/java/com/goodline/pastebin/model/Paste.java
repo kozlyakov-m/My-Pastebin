8dac11954ea0005e19266d0447bc7f2d57863fca
kozlyakov.m <kozlyakov.m@gmail.com>
2020-06-06 01:30:41 +0700

    Paste can be long text

--- src/main/java/com/goodline/pastebin/model/Paste.java
+++ src/main/java/com/goodline/pastebin/model/Paste.java
@@ -11,6 +11,7 @@ public class Paste{
 
     @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
+    @Lob
     private String text;
     private String hash;
     private Date expireDate;
---
+++ src/main/resources/db/migration/V6__Alter_text_column.sql
@@ -0,0 +1,1 @@
+alter table paste alter column text text;
