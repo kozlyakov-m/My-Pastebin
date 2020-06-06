c01fa58fcba722b138c7329486bdb18d54e31949
kozlyakov.m <kozlyakov.m@gmail.com>
2020-06-06 15:57:16 +0700

    Encode user pass

--- src/main/resources/db/migration/V4__Add_user.sql
+++ src/main/resources/db/migration/V4__Add_user.sql
@@ -1,1 +1,1 @@
-insert into users(login, password, role) values ('user', 'pass', 'USER');
+insert into users(login, password, role) values ('user', '$2y$12$6MwOk3eIqGsTtBS4Fye1DO58BAkCHsE6VNX.OsOEVY28JPninvy8e', 'USER');
