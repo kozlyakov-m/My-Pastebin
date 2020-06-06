80e2430361f465a7cb653cbb3dc6827e1c70153a
kozlyakov.m <kozlyakov.m@gmail.com>
2020-06-06 16:29:30 +0700

    Delete and insert test pastes

---
+++ src/main/resources/db/migration/V8__Delete_and_insert_test_pastes.sql
@@ -0,0 +1,14 @@
+delete from paste;
+
+insert into paste (author, expire_date, hash, text, type)
+values ('user', '2020-12-31', 'caf2421e-1580-4e06-9bc0-a82e28620072', 'text 1', 0);
+
+insert into paste (author, expire_date, hash, text, type)
+values ('user', '2020-12-31', 'cc6c9792-3c64-4f97-8cac-b00683ab45ac', 'text 2', 0);
+
+insert into paste (author, expire_date, hash, text, type)
+values (null, '2020-12-31', '02256c7e-a4ea-4202-a196-973b345c2e6e', 'text 3', 1);
+
+insert into paste (author, expire_date, hash, text, type)
+values ('user', '2020-12-31', '0f1b209a-97bc-4981-852d-de93e7050632', 'text 4', 2);
+
