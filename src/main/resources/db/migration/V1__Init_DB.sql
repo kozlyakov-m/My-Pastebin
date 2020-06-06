fefd12c662daf1ba4133f421b21185897c190d96
kozlyakov.m <kozlyakov.m@gmail.com>
2020-06-06 14:45:43 +0700

    Postgresql migration

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,7 +1,7 @@
 create sequence hibernate_sequence start 1 increment 1;
 
 create table paste (
-id integer not null auto_increment,
+id serial,
 author varchar(255),
 expire_date timestamp,
 hash varchar(255),
